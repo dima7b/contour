7ca5299bebb345245f0890574a70230ad17b2fd1
Dmitriy Bernasovskiy <dmitriybernasovskiy@Dmitriys-MacBook-Pro.local>
2016-01-05 13:39:41 -0800

    Init
    - Main view
    - Materials view

---
+++ assets/html/components/navbar-materials.php
@@ -0,0 +1,27 @@
+<nav class="green accent-3" role="navigation">
+  <div class="nav-wrapper">
+    <!-- <a id="logo-container" href="#" class="brand-logo">Logo</a> -->
+    <ul class="left hide-on-med-and-down">
+      <li><a href="#"># Orders</a></li>
+      <li><a href="#">/ Products</a></li>
+      <li class="active"><a href="materials.php">[ Materials</a></li>
+      <li><form>
+        <div class="input-field">
+          <input id="search" type="search" placeholder="Search for materials, Products, &amp; Projects" required>
+          <label for="search"><i class="material-icons">search</i></label>
+          <i class="material-icons">close</i>
+        </div>
+      </form></li>
+    </ul>
+
+    <ul class="right hide-on-med-and-down">
+      <li class="apps"><a href="#"><i class="large material-icons">apps</i> Apps</a></li>
+      <li class="profile"><a href="#"><i class="large material-icons">face</i></a></li>
+    </ul>
+
+    <ul id="nav-mobile" class="side-nav">
+      <li><a href="#">Navbar Link</a></li>
+    </ul>
+    <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+  </div>
+</nav>
