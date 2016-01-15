c811840fe208065b0865dcd698e899e3a090c505
Dmitriy Bernasovskiy <dmitriybernasovskiy@Dmitriys-MacBook-Pro.local>
2016-01-14 17:01:02 -0800

    Materials Flow 18

---
+++ materials-flow-18.php
@@ -0,0 +1,106 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>Contour</title>
+
+  <link href="//fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="assets/css/main.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+
+
+  <?php include 'assets/html/components/navbar-materials.php' ?>
+
+
+  <div class="container">
+    <div class="collection-wrapper">
+
+      <ul class="collection expanded z-depth-1">
+        <p class="sub-title"><strong>6010 Round</strong></p>
+        <li class="collection-item collection-item-expanded z-depth-1">
+
+          <div class="row row-header">
+            <div class="col s8">
+              <a href="#"><i class="material-icons">close</i></a>
+            </div>
+            <div class="col s4 right-align">
+              <a href="#"><i class="material-icons">insert_link</i></a>
+              <a href="#"><i class="material-icons">done</i></a>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="details col s12">
+              <div class="row">
+                <div class="col s3">
+                  <p class="gray">Broken down as:</p>
+                </div>
+                <div class="col s4">
+                  <p>Fractions</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="details col s12">
+              <div class="row">
+                <div class="col s3">
+                  <p class="gray">Measured by:</p>
+                </div>
+                <div class="col s4">
+                  <p>Feet</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+          <ul class="collection expanded sub-details">
+            <li>
+              <div class="row">
+                <div class="col s4">
+                  <p class="gray">Fractions of:</p>
+                </div>
+                <div class="col s1"><p class="unit">1</p></div>
+                <div class="col s1"><p class="unit">1/2</p></div>
+                <div class="col s1"><p class="unit">1/4</p></div>
+                <div class="col s1"><p class="unit">1/10</p></div>
+                <div class="col s1"><p class="unit">1/12</p></div>
+              </div>
+            </li>
+            <li>
+              <div class="row">
+                <div class="col s6">
+                  <p class="gray">Maximum measurement of</p>
+                </div>
+                <div class="col s1"><p>ft</p></div>
+              </div>
+            </li>
+            <li>
+              <div class="row">
+                <div class="col s6">
+                  <p class="gray">Minimum measurement of</p>
+                </div>
+                <div class="col s1"><p>ft</p></div>
+              </div>
+            </li>
+          </ul>
+          </div>
+
+        </li>
+      </ul>
+
+    </div>
+  </div>
+
+
+  <!--  Scripts-->
+  <script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="assets/js/main.js"></script>
+
+  </body>
+</html>
