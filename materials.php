83cf871bcb3235cd54ada202fab5b90bed8b758e
Dmitriy Bernasovskiy <dmitriybernasovskiy@Dmitriys-MacBook-Pro.local>
2016-01-18 02:01:16 -0800

    Materials Flow 9

--- materials.php
+++ materials.php
@@ -417,6 +417,71 @@
         </li>
       </ul>
 
+      <br><h6>Materials Flow 9</h6>
+      <!-- Materials Flow 9 -->
+      <ul class="collection expanded z-depth-1">
+        <p class="sub-title"><strong>Balusters</strong></p>
+
+        <ul class="collection expanded z-depth-1">
+          <p class="sub-title"><strong>5015-36-O</strong></p>
+
+          <li class="collection-item collection-item-expanded z-depth-1">
+
+            <div class="row row-header">
+              <div class="col s8">
+                <a href="#"><i class="material-icons">close</i></a>
+              </div>
+              <div class="col s4 right-align">
+                <a href="#"><i class="material-icons">insert_link</i></a>
+                <a href="#"><i class="material-icons">done</i></a>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="details col s12">
+                <div class="row">
+                  <div class="col s4">
+                    <p class="gray">Broken down as:</p>
+                  </div>
+                  <div class="col s4">
+                    <p>Fractions</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="row">
+              <ul class="collection expanded sub-details">
+                <li>
+                  <div class="row">
+                    <div class="col s4">
+                      <p class="gray">Measured in:</p>
+                    </div>
+                    <div class="col s3"><p class="unit">Measured in:</p></div>
+                    <div class="col s3"><p class="unit">Measured in:</p></div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+
+          </li>
+        </ul>
+
+        <ul class="collection z-depth-1">
+          <li class="collection-item typing">
+            <div class="row">
+              <div class="col s8">
+                <p>5015-39-O</p>
+              </div>
+              <div class="col s4 right-align">
+                <p class="being-typed">[+ details sheet]</p>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </ul>
+
+
     </div>
   </div>
 
