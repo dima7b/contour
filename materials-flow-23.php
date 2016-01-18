266ad4d847cbc80eedf88428b61a745bb37c96b2
Dmitriy Bernasovskiy <dmitriybernasovskiy@Dmitriys-MacBook-Pro.local>
2016-01-18 01:58:24 -0800

    Materials Flow 23

---
+++ materials-flow-23.php
@@ -0,0 +1,64 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>Contour</title>
+
+  <link href="//fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="assets/css/main.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+
+
+  <?php include 'assets/html/components/navbar-materials.php' ?>
+
+
+  <div class="container">
+    <div class="collection-wrapper">
+
+      <!-- Materials Flow 23 -->
+      <ul class="collection expanded z-depth-1">
+        <p class="sub-title"><strong>6010 Round</strong></p>
+        <li class="collection-item collection-item-expanded z-depth-1">
+
+          <div class="row row-header">
+            <div class="col s8">
+              <a href="#"><i class="material-icons">close</i></a>
+            </div>
+            <div class="col s4 right-align">
+              <a href="#"><i class="material-icons">insert_link</i></a>
+              <a href="#"><i class="material-icons">done</i></a>
+            </div>
+          </div>
+
+          <div class="row">
+            <ul class="collection expanded sub-details">
+              <li>
+                <div class="row">
+                  <div class="col s4">
+                    <p class="gray">Measured by:</p>
+                  </div>
+                  <div class="col s2"><p class="unit">Amount</p></div>
+                  <div class="col s2"><p class="unit">Feet</p></div>
+                  <div class="col s2"><p class="unit">Meters</p></div>
+                  <div class="col s2"><p class="unit">Pounds</p></div>
+                </div>
+              </li>
+            </ul>
+          </div>
+
+        </li>
+      </ul>
+
+    </div>
+  </div>
+
+
+  <!--  Scripts-->
+  <script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="assets/js/main.js"></script>
+
+  </body>
+</html>
--- materials.php
+++ materials.php
@@ -382,6 +382,40 @@
         </ul>
       </ul>
 
+      <br><h6>Materials Flow 23</h6>
+      <!-- Materials Flow 23 -->
+      <ul class="collection expanded z-depth-1">
+        <p class="sub-title"><strong>6010 Round</strong></p>
+        <li class="collection-item collection-item-expanded z-depth-1">
+
+          <div class="row row-header">
+            <div class="col s8">
+              <a href="#"><i class="material-icons">close</i></a>
+            </div>
+            <div class="col s4 right-align">
+              <a href="#"><i class="material-icons">insert_link</i></a>
+              <a href="#"><i class="material-icons">done</i></a>
+            </div>
+          </div>
+
+          <div class="row">
+            <ul class="collection expanded sub-details">
+              <li>
+                <div class="row">
+                  <div class="col s4">
+                    <p class="gray">Measured by:</p>
+                  </div>
+                  <div class="col s2"><p class="unit">Amount</p></div>
+                  <div class="col s2"><p class="unit">Feet</p></div>
+                  <div class="col s2"><p class="unit">Meters</p></div>
+                  <div class="col s2"><p class="unit">Pounds</p></div>
+                </div>
+              </li>
+            </ul>
+          </div>
+
+        </li>
+      </ul>
 
     </div>
   </div>
