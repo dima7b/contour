55628c6b73042426b3fd8e484ae75006889b2e8d
Dmitriy Bernasovskiy <dmitriybernasovskiy@Dmitriys-MacBook-Pro.local>
2016-01-18 02:34:51 -0800

    Restructing

---
+++ assets/html/components/snippet-head.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>Contour</title>
+
+  <link href="//fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="assets/css/main.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
--- materials-flow-23.php
+++ materials-flow-23.php
@@ -1,18 +1,5 @@
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
-  <title>Contour</title>
-
-  <link href="//fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link href="assets/css/main.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-</head>
-<body>
-
-
-  <?php include 'assets/html/components/navbar-materials.php' ?>
+<?php include 'assets/html/components/snippet-head.php' ?>
+<?php include 'assets/html/components/navbar-materials.php' ?>
 
 
   <div class="container">
@@ -56,9 +43,4 @@
   </div>
 
 
-  <!--  Scripts-->
-  <script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
-  <script src="assets/js/main.js"></script>
-
-  </body>
-</html>
+<?php include 'assets/html/components/snippet-head.php' ?>
