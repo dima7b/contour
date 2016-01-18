f43d8d4142b56894596bcc4ca2f67ec05247df5a
Dmitriy Bernasovskiy <dmitriybernasovskiy@Dmitriys-MacBook-Pro.local>
2016-01-18 01:53:20 -0800

    Materials Flow 12

---
+++ materials-flow-12.php
@@ -0,0 +1,96 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>Contour</title>
+
+  <link href="//fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="assets/css/main.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+
+
+  <?php include 'assets/html/components/navbar-materials.php' ?>
+
+
+  <div class="container">
+    <div class="collection-wrapper">
+
+      <!-- Materials Flow 12 -->
+
+      <ul class="collection expanded z-depth-1">
+        <p class="sub-title"><strong>Balusters</strong></p>
+
+        <ul class="collection expanded z-depth-1">
+          <p class="sub-title"><strong>5015-36-O</strong></p>
+
+          <li class="collection-item collection-item-expanded z-depth-1">
+
+            <div class="row row-header">
+              <div class="col s8">
+                <a href="#"><i class="material-icons">close</i></a>
+              </div>
+              <div class="col s4 right-align">
+                <a href="#"><i class="material-icons">insert_link</i></a>
+                <a href="#"><i class="material-icons">done</i></a>
+              </div>
+            </div>
+
+            <div class="row">
+              <ul class="collection expanded sub-details">
+                <li>
+                  <div class="row">
+                    <div class="col s4">
+                      <p class="gray">Measured in:</p>
+                    </div>
+                    <div class="col s3"><p class="unit">Measured in:</p></div>
+                    <div class="col s3"><p class="unit">Measured in:</p></div>
+                  </div>
+                </li>
+                <li>
+                  <div class="row">
+                    <div class="col s4">
+                      <p class="gray">of:</p>
+                    </div>
+                    <div class="col s2"><p class="unit">Feet</p></div>
+                    <div class="col s2"><p class="unit">Meters</p></div>
+                    <div class="col s2"><p class="unit">Pounds</p></div>
+                    <div class="col s2"><p class="unit">Gallons</p></div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+
+          </li>
+        </ul>
+
+        <ul class="collection z-depth-1">
+          <li class="collection-item typing">
+            <div class="row">
+              <div class="col s8">
+                <p>5015-36-O <span class="gray" style="padding-left:30px;">Exact Amounts, Quantity</span></p>
+              </div>
+              <div class="col s4 right-align">
+                <p class="being-typed">
+                  <i class="material-icons">wallpaper</i></a>
+                  <i class="material-icons">restore</i></a>
+                  <i class="material-icons">create</i></a>
+                </p>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </ul>
+
+    </div>
+  </div>
+
+
+  <!--  Scripts-->
+  <script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="assets/js/main.js"></script>
+
+  </body>
+</html>
--- materials.php
+++ materials.php
@@ -19,7 +19,7 @@
     <div class="collection-wrapper">
 
       <br><h6>Materials Flow 1</h6>
-      <!-- Materials FLow 1 -->
+      <!-- Materials Flow 1 -->
       <ul class="collection z-depth-1">
         <li class="collection-item empty">
           <div class="row">
@@ -31,7 +31,7 @@
       </ul>
 
       <br><h6>Materials Flow 2</h6>
-      <!-- Materials FLow 2 -->
+      <!-- Materials Flow 2 -->
       <ul class="collection z-depth-1">
         <li class="collection-item typing">
           <div class="row">
@@ -46,7 +46,7 @@
       </ul>
 
       <br><h6>Materials Flow 17</h6>
-      <!-- Materials FLow 17 -->
+      <!-- Materials Flow 17 -->
       <ul class="collection z-depth-1">
         <li class="collection-item typing">
           <div class="row">
@@ -68,7 +68,7 @@
       </ul>
 
       <br><h6>Materials Flow 11</h6>
-      <!-- Materials FLow 11 -->
+      <!-- Materials Flow 11 -->
       <ul class="collection z-depth-1">
         <li class="collection-item typing">
           <div class="row">
@@ -104,7 +104,7 @@
       </ul>
 
       <br><h6>Materials Flow 21</h6>
-      <!-- Materials FLow 21 -->
+      <!-- Materials Flow 21 -->
       <ul class="collection z-depth-1">
         <li class="collection-item typing">
           <div class="row">
@@ -125,7 +125,7 @@
       </ul>
 
       <br><h6>Materials Flow 4</h6>
-      <!-- Materials FLow 4 -->
+      <!-- Materials Flow 4 -->
       <ul class="collection expanded z-depth-1">
         <p class="sub-title"><strong>Balusters</strong></p>
         <li class="collection-item z-depth-1">
@@ -141,7 +141,7 @@
       </ul>
 
       <br><h6>Materials Flow 18</h6>
-      <!-- Materials FLow 18 -->
+      <!-- Materials Flow 18 -->
       <ul class="collection expanded z-depth-1">
         <p class="sub-title"><strong>6010 Round</strong></p>
         <li class="collection-item collection-item-expanded z-depth-1">
@@ -219,7 +219,7 @@
       </ul>
 
       <br><h6>Materials Flow 6</h6>
-      <!-- Materials FLow 6 -->
+      <!-- Materials Flow 6 -->
       <ul class="collection expanded z-depth-1">
         <p class="sub-title"><strong>6010 Round</strong></p>
         <li class="collection-item collection-item-expanded z-depth-1">
@@ -254,7 +254,7 @@
 
 
       <br><h6>Materials Flow 8</h6>
-      <!-- Materials FLow 8 -->
+      <!-- Materials Flow 8 -->
       <ul class="collection expanded z-depth-1">
         <p class="sub-title"><strong>Balusters</strong></p>
 
@@ -313,7 +313,73 @@
             </div>
           </li>
         </ul>
+      </ul>
 
+      <br><h6>Materials Flow 12</h6>
+      <!-- Materials Flow 12 -->
+
+      <ul class="collection expanded z-depth-1">
+        <p class="sub-title"><strong>Balusters</strong></p>
+
+        <ul class="collection expanded z-depth-1">
+          <p class="sub-title"><strong>5015-36-O</strong></p>
+
+          <li class="collection-item collection-item-expanded z-depth-1">
+
+            <div class="row row-header">
+              <div class="col s8">
+                <a href="#"><i class="material-icons">close</i></a>
+              </div>
+              <div class="col s4 right-align">
+                <a href="#"><i class="material-icons">insert_link</i></a>
+                <a href="#"><i class="material-icons">done</i></a>
+              </div>
+            </div>
+
+            <div class="row">
+              <ul class="collection expanded sub-details">
+                <li>
+                  <div class="row">
+                    <div class="col s4">
+                      <p class="gray">Measured in:</p>
+                    </div>
+                    <div class="col s3"><p class="unit">Measured in:</p></div>
+                    <div class="col s3"><p class="unit">Measured in:</p></div>
+                  </div>
+                </li>
+                <li>
+                  <div class="row">
+                    <div class="col s4">
+                      <p class="gray">of:</p>
+                    </div>
+                    <div class="col s2"><p class="unit">Feet</p></div>
+                    <div class="col s2"><p class="unit">Meters</p></div>
+                    <div class="col s2"><p class="unit">Pounds</p></div>
+                    <div class="col s2"><p class="unit">Gallons</p></div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+
+          </li>
+        </ul>
+
+        <ul class="collection z-depth-1">
+          <li class="collection-item typing">
+            <div class="row">
+              <div class="col s8">
+                <p>5015-36-O <span class="gray" style="padding-left:30px;">Exact Amounts, Quantity</span></p>
+              </div>
+              <div class="col s4 right-align">
+                <p class="being-typed">
+                  <i class="material-icons">wallpaper</i></a>
+                  <i class="material-icons">restore</i></a>
+                  <i class="material-icons">create</i></a>
+                </p>
+              </div>
+            </div>
+          </li>
+        </ul>
       </ul>
 
 
