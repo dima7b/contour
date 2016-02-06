bd18fa43bc47e5f30d580272bc0e0999f1b950c1
Dmitriy Bernasovskiy <dmitriybernasovskiy@Dmitriys-MacBook-Pro.local>
2016-02-06 07:51:19 -0700

    Enter button changes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,29 @@
 $(function(){
 
+  var newItem = '<li class="collection-item empty"> <div class="row"> <div class="col s7"> <textarea name="enter_type" placeholder="Start typing or tap to add Material Category" rows="10" cols="15"></textarea> </div> <div class="col s5 right-align tap-done"> <p class="being-typed done" style="display: none;">Tap enter when <span>DONE</span></p> <p class="being-details"><a href="#">+ details</a></p> </div> </div> </li>';
+
+
+  $("textarea").keypress(function (e) {
+    if (e.keyCode == 13) {
+      $('.collection').append(newItem);
+      $('.collection-item .tap-done .done').css('display', 'none');
+      $('.collection-item .tap-done .being-details').show();
+      $(this).attr('readonly','readonly');
+    }
+  });
+
+  $('textarea').keypress(function(e){
+     if (e.keyCode == 13) return false
+  });
+
   $('.materials-btn').click(function(){
-    var newItem = '<li class="collection-item empty"> <div class="row"> <div class="col s7"> <textarea name="enter_type" placeholder="Start typing or tap to add Material Category" rows="10" cols="15"></textarea> </div> <div class="col s5 right-align tap-done"> <p class="being-typed">Tap enter when <span>DONE</span></p> </div> </div> </li>';
     $('.collection').append(newItem);
   });
 
   $('.collection-item.empty').click(function(evt) {
       evt.stopPropagation(); //stops the document click action
-      $(this).toggleClass('empty');
-      $(this).toggleClass('active-item');
+      $(this).removeClass('empty');
+      $(this).addClass('active-item');
   });
 
   $(document).click(function() {
@@ -22,17 +37,6 @@ $(function(){
     } else {
       $('.collection-item .tap-done .done').hide();
     }
-  });
-
-  $("textarea").keypress(function (e) {
-    if (e.keyCode == 13) {
-      var newItem = '<li class="collection-item empty"> <div class="row"> <div class="col s7"> <textarea name="enter_type" placeholder="Start typing or tap to add Material Category" rows="10" cols="15"></textarea> </div> <div class="col s5 right-align tap-done"> <p class="being-typed">Tap enter when <span>DONE</span></p> </div> </div> </li>';
-      $('.collection').append(newItem);
-    }
-  });
-
-  $('textarea').keypress(function(e){
-     if (e.keyCode == 13) return false
   });
 
 });
